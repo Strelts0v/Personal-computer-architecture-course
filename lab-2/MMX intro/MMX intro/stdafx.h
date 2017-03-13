68ea213de458d6242303401fb4a9cdaf386c45dd
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-03-13 23:38:20 +0300

    added axiliary headers

---
+++ lab-2/MMX intro/MMX intro/stdafx.h
@@ -0,0 +1,23 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <conio.h>
+#include <ctime>
+
+// addtional custom headers
+#include "Matrix.h"
+#include "MatrixMutator.h"
+#include "MatrixFactory.h"
+#include "AsmMatrixMutator.h"
+#include "CMatrixMutator.h"
+#include "MmxMatrixMutator.h"
+
+#define MATRIX_DIMENSION 8
